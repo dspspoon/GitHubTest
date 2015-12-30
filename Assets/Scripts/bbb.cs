1b8b682304dbb73824650b42a717581191d2655c
dspspoon <dspspoon@naver.com>
2015-12-30 10:38:17 +0900

    modified aaa.cs add bbb.cs

--- Assets/Scripts/bbb.cs
+++ Assets/Scripts/bbb.cs
@@ -1,12 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class aaa : MonoBehaviour {
+public class bbb : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        int a = 0;
-        int b = 0;
+	
 	}
 	
 	// Update is called once per frame
